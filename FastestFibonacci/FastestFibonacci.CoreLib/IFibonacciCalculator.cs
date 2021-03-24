0e6de9e4d33785e63e39bd17d21db8abca713254
Josh Coleman <josh.coleman82@gmail.com>
2021-03-24 14:42:26 -0700

    Create Recursive implementation

--- FastestFibonacci/FastestFibonacci.CoreLib/IFibonacciCalculator.cs
+++ FastestFibonacci/FastestFibonacci.CoreLib/IFibonacciCalculator.cs
@@ -2,8 +2,18 @@
 
 namespace FastestFibonacci.CoreLib
 {
+    /// <summary>
+    /// Basic inferface to abstract different Fibonacci implementations.
+    /// See: https://en.wikipedia.org/wiki/Fibonacci_number for major concepts
+    /// </summary>
     public interface IFibonacciCalculator
     {
-        UInt64 CalculateFibonacci(UInt64 input);
+
+        /// <summary>
+        /// Calculates the specified fibonacci value.
+        /// </summary>
+        /// <param name="input">the desired fibonacci value to be calculated</param>
+        /// <returns>the requested fibonacci number</returns>
+        ulong CalculateFibonacci(ulong input);
     }
 }
---
+++ FastestFibonacci/FastestFibonacci.CoreLib/RecursiveFibonacciImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastestFibonacci.CoreLib
+{
+    /// <summary>
+    /// Recursive implementation for Fibonacci generation
+    /// </summary>
+    public class RecursiveFibonacciImpl : IFibonacciCalculator
+    {
+        /// <summary>
+        /// Recursively calculates Fibonacci
+        /// </summary>
+        /// <param name="input">desired fibonacci value</param>
+        /// <returns>result of calculation</returns>
+        public ulong CalculateFibonacci(ulong input)
+        {
+            if(input == 0)
+            {
+                return 0;
+            }
+            else if(input == 1)
+            {
+                return 1;
+            }
+            return CalculateFibonacci(input - 1) + CalculateFibonacci(input - 2);
+        }
+    }
+}
